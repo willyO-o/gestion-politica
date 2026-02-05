b86480f8a2833c1d38967d69143f3a5f812be3a0
willyO-o <marcggmarc.20@gmail.com>
2026-02-05 19:06:30 -0400

    subiendo correcciones

--- config/cors.php
+++ config/cors.php
@@ -25,10 +25,10 @@ return [
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+    'exposed_headers' => ['Authorization'],
 
-    'max_age' => 0,
+    'max_age' => 86400,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
